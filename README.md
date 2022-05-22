95636c8c4b6290f00b1af09a5f5d092954c3b4bc
wahyurahmana <tn.wahyurahmana@gmail.com>
2022-05-22 22:40:12 +0800

    sample code upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Potongan Kode Belajar Upload
+## Potongan kode ini bertujuan untuk belajar bagaimana cara mengupload file dengan express & nodejs & multer
+### Adapun fiturnya yaitu : batas ukuran file, type file yang diizinkan dan mengembalikan nama file beserta extensi nya pada folder (folder upload harus dibuat dulu)
+
+#### note : jangan lupa install package nya ya
---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const multer = require('multer')
+const app = express()
+const port = 3000
+
+app.use(express.static('upload'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './upload')
+  },
+  filename: (req, file, cb) => {
+    const nameFile = Math.ceil(Math.random() * 999) + file.originalname
+    cb(null, nameFile)
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+      cb(null, true)
+    } else {
+      cb(new Error('Terjadi Kesalahan'))
+    }
+  },
+  limits : {
+    fileSize : 300000
+  }
+})
+
+app.post('/upload-single', upload.single('fieldUpload'), (req, res) => {
+  res.status(200).json({
+    message: 'berhasil upload file'
+  })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err.message
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
